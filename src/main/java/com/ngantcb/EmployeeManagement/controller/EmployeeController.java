22940ee6c6505624cb9cb2a237c01199761ca678
LisaTheTechnologue <jolisa.chan.store@gmail.com>
2023-04-24 21:49:26 +0700

    Add error message

--- src/main/java/com/ngantcb/EmployeeManagement/controller/EmployeeController.java
+++ src/main/java/com/ngantcb/EmployeeManagement/controller/EmployeeController.java
@@ -1,13 +1,15 @@
 package com.ngantcb.EmployeeManagement.controller;
 
 import com.ngantcb.EmployeeManagement.entity.Employee;
-import com.ngantcb.EmployeeManagement.repository.EmployeeRepository;
 import com.ngantcb.EmployeeManagement.service.EmployeeService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EmployeeController {
@@ -28,20 +30,32 @@ public class EmployeeController {
         return "create";
     }
 
-    @PostMapping("/save")
-    public String addEmployee(@ModelAttribute("employee") Employee employee) {
-        service.save(employee);
+    @PostMapping("/create")
+    public String addEmployee(@ModelAttribute("employee") @Valid Employee employee,
+                              Model model) {
+        try {
+            service.save(employee);
+        } catch (Exception ex) {
+            model.addAttribute("error", "failed");
+            return "create";
+        }
         return "redirect:/";
-
     }
 
     @GetMapping("/delete/{id}")
-    public String deleteEmployee(@PathVariable(value = "id") Long id) {
-        service.delete(id);
+    public String deleteEmployee(@PathVariable(value = "id") Long id, Model model) {
+        try {
+            service.delete(id);
+        } catch (Exception ex) {
+            model.addAttribute("error", "failed");
+            model.addAttribute("employees", service.getAll());
+            return "index";
+        }
         return "redirect:/";
     }
+
     @GetMapping("/details/{id}")
-    public String getEmployeeById(@PathVariable(value = "id") Long id,Model model) {
+    public String getEmployeeById(@PathVariable(value = "id") Long id, Model model) {
         Employee employee = service.get(id);
         model.addAttribute("emp", employee);
         return "details";
